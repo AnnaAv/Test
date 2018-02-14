52c4a70e9dcb0600e7959439f01930f5879b24e2
Anna Avetisyan <anna@gmail.com>
2018-02-15 01:27:29 +0400

    initial

---
+++ app/src/main/java/ru/startandroid/p004_basicviews/MainActivity.java
@@ -0,0 +1,13 @@
+package ru.startandroid.p004_basicviews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.myscreen);
+    }
+}
